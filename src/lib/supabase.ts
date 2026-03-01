8f46d7731e857da656d628582c5a79e6a1a32717
Estevão Ferreira Machado <compass-estevao.machado@ailos.coop.br>
2026-03-01 02:48:51 -0300

    Alteração de credenciais

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://zyahuyqjlkzvvixoxwgq.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5YWh1eXFqbGt6dnZpeG94d2dxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NzM3ODEsImV4cCI6MjA4NTE0OTc4MX0.SGCSzYUev7YYrOOPS2kmfW6935UMo2E2LpI3SQwHEJo';
+const SUPABASE_URL = process.env.SUPABASE_URL ?? '';
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY ?? '';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
